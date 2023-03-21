04f5a7d8faef1eb1c47095ffc05f5f186e49b3ab
Iain Chambers <iainjchambers@gmail.com>
2023-03-21 16:37:53 +0000

    Stage 2
    Added text that shows the total using a simple array.reduce() function

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,14 +1,17 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./";
 
-test("should show three initial zeros, then a one when a counter is incremented then three zeros again when the same counter is decremented", () => {
+test("should show three initial zeros, then a one when a counter is incremented then three zeros again when the same counter is decremented and updates the total at each step", () => {
   render(<App />);
 
   expect(screen.getAllByText("0").length).toEqual(3);
+  expect(screen.getByText("Total: 0")).toBeTruthy();
 
   fireEvent.click(screen.getAllByText("+")[1]);
   expect(screen.getAllByText("1").length).toEqual(1);
+  expect(screen.getByText("Total: 1")).toBeTruthy();
 
   fireEvent.click(screen.getAllByText("-")[1]);
   expect(screen.getAllByText("0").length).toEqual(3);
+  expect(screen.getByText("Total: 0")).toBeTruthy();
 });
--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -13,29 +13,34 @@ export default function App() {
   const [counters, setCounters] = useState(data);
 
   return (
-    <div>
-      {counters.map(({ id, value }) => (
-        <Counter
-          key={id}
-          value={value}
-          increment={() => {
-            counters[
-              counters.findIndex((counter) => counter.id === id)
-            ].value += 1;
+    <>
+      <div>
+        {counters.map(({ id, value }) => (
+          <Counter
+            key={id}
+            value={value}
+            increment={() => {
+              counters[
+                counters.findIndex((counter) => counter.id === id)
+              ].value += 1;
 
-            setCounters([...counters]);
-          }}
-          decrement={() => {
-            // Might not want this to go below 0 so could
-            // conditionally return if the value is already at 0
-            counters[
-              counters.findIndex((counter) => counter.id === id)
-            ].value -= 1;
+              setCounters([...counters]);
+            }}
+            decrement={() => {
+              // Might not want this to go below 0 so could
+              // conditionally return if the value is already at 0
+              counters[
+                counters.findIndex((counter) => counter.id === id)
+              ].value -= 1;
 
-            setCounters([...counters]);
-          }}
-        />
-      ))}
-    </div>
+              setCounters([...counters]);
+            }}
+          />
+        ))}
+      </div>
+      <div>
+        Total: {counters.reduce((acc, counter) => acc + counter.value, 0)}
+      </div>
+    </>
   );
 }
